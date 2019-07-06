e8bce8af1d2bab648c9c38edea1f0558a0bb2677
Freeman <freeman.cho@gmail.com>
2019-07-06 17:49:41 +0900

    init

--- FreeTodo5/app/src/main/java/com/freeman/freetodo5/utils/db/GlobalVariable.java
+++ FreeTodo5/app/src/main/java/com/freeman/freetodo5/utils/db/GlobalVariable.java
@@ -1,4 +1,29 @@
 package com.freeman.freetodo5.utils.db;
 
 public class GlobalVariable {
+    private static GlobalVariable instance = null;
+
+    private boolean mSideMenuFavoriteChange = true;
+    private boolean mSideMenuItemsChange = true;
+
+    public boolean isSideMenuFavoriteChange() {
+        return mSideMenuFavoriteChange;
+    }
+
+    public void setSideMenuFavoriteChange(boolean mSideMenuFavoriteChange) {
+        this.mSideMenuFavoriteChange = mSideMenuFavoriteChange;
+    }
+
+    public boolean isSideMenuItemsChange() {
+        return mSideMenuItemsChange;
+    }
+
+    public void setSideMenuItemsChange(boolean mSideMenuItemsChange) {
+        this.mSideMenuItemsChange = mSideMenuItemsChange;
+    }
+
+    public static synchronized GlobalVariable getInstance() {
+        if (instance == null) instance = new GlobalVariable();
+        return instance;
+    }
 }
